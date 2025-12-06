017b5aa8e8411f43cb64de5c1336fbd7124af934
Daniel <daniel.k.bostwick@gmail.com>
2025-12-05 16:20:29 -0800

    random eval runs

--- commands/eval.sh
+++ commands/eval.sh
@@ -2,6 +2,7 @@
 lerobot-eval \
   --policy.path="adalal/smolvla-libero-64-2.0" \
   --policy.n_action_steps=10 \
+  --policy.auto_patch_random=false \
   --policy.auto_patch_target="TARGET_WORD" \
   --policy.auto_patch_k=NUM_VECTORS_TO_PATCH \
   --policy.auto_patch_alpha=PATCH_STRENGTH \
@@ -17,6 +18,7 @@ lerobot-eval \
 lerobot-eval \
   --policy.path="lerobot/xvla-libero" \
   --policy.n_action_steps=10 \
+  --policy.auto_patch_random=false \
   --policy.auto_patch_target="TARGET_WORD" \
   --policy.auto_patch_k=NUM_VECTORS_TO_PATCH \
   --policy.auto_patch_alpha=PATCH_STRENGTH \
@@ -28,3 +30,13 @@ lerobot-eval \
   --eval.n_episodes=5 \
   --eval.condition_label=RUN_ID \
   --output_dir=./logs/RUN_ID
+
+  lerobot-eval \
+  --policy.path="adalal/smolvla-libero-64-2.0" \
+  --policy.n_action_steps=10 \
+  --env.type=libero \
+  --env.task=libero_object \
+  --eval.batch_size=5 \
+  --eval.n_episodes=5 \
+  --eval.condition_label=smolvla-down-prepend \
+  --output_dir=./logs/smolvla-down-prepend
